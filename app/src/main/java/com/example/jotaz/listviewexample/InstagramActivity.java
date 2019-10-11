7fa8d0035e4777cb25292a429986595a4bac616b
eliecer <elieaguilar91@gmail.com>
2019-10-11 03:15:50 -0500

    creacion de menu en action bar, correccion de errores

--- app/src/main/java/com/example/jotaz/listviewexample/InstagramActivity.java
+++ app/src/main/java/com/example/jotaz/listviewexample/InstagramActivity.java
@@ -2,6 +2,8 @@ package com.example.jotaz.listviewexample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -38,9 +40,9 @@ public class InstagramActivity extends AppCompatActivity {
     }
 
     private void templateListView(){
-        List<Chat> opc = this.GetElementsToListViewTemplate();
+        List<Chat> opc1 = this.GetElementsToListViewTemplate();
 
-        InstagramListviewAdapter adapter = new InstagramListviewAdapter(this, opc);
+        InstagramListviewAdapter adapter = new InstagramListviewAdapter(this, opc1);
 
         listaChats.setAdapter(adapter);
     }
@@ -49,12 +51,38 @@ public class InstagramActivity extends AppCompatActivity {
     {
         List<Chat> opc = new ArrayList<>();
 
-        opc.add(new Chat("Eliecer", "Hola como estas", "10 AM",R.drawable.user1));
-        opc.add(new Chat("Maria", "Pasame el problema 3","12 AM",R.drawable.usr2));
-        opc.add(new Chat("Carlos", "Que dieron en HP4 ?","1:50 PM",R.drawable.usr3));
-        opc.add(new Chat("Andrea", "Recuerden el A3 para el miercoles","11:59 PM",R.drawable.usr4));
+        opc.add(new Chat("Eliecer", "Hola como estas", "10 AM",R.drawable.ico));
+        opc.add(new Chat("Maria", "Pasame el problema 3","12 AM",R.drawable.ic02));
+        opc.add(new Chat("Carlos", "Que dieron en HP4 ?","1:50 PM",R.drawable.uc01));
+        opc.add(new Chat("Andrea", "Recuerden el A3 para el miercoles","11:59 PM",R.drawable.ic03));
 
         return  opc;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_instagram,menu);
+        return  true;
+
+    }
+
+
+    public boolean onOptionItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case R.id.newItem:
+                this.AgregarNuevoElemento();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+    private void AgregarNuevoElemento(){
+        List<Chat> opc = new ArrayList<>();
+        opc.add(new Chat("Eliecer", "Hola como estas", "10 AM",R.drawable.ico));
+        InstagramListviewAdapter adapter = new InstagramListviewAdapter(this, opc);
+        listaChats.setAdapter(adapter);
     }
 
 }
