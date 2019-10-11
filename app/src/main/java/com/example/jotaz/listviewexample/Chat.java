6dac8aecb478c5366f513ca0102c0d76120ec153
eliecer <elieaguilar91@gmail.com>
2019-10-11 01:53:57 -0500

    clase creadas: chats , instagram listview adapter, modificaion de instagramActivity

--- app/src/main/java/com/example/jotaz/listviewexample/Chat.java
+++ app/src/main/java/com/example/jotaz/listviewexample/Chat.java
@@ -1,5 +1,32 @@
 package com.example.jotaz.listviewexample;
 
 public class Chat {
+    private String user;
+    private String msj;
+    private String h;
+    private int img;
 
+    public Chat(String usuario, String mensaje, String hora, int imagen) {
+        this.user = usuario;
+        this.msj = mensaje;
+        this.h = hora;
+        this.img = imagen;
+    }
+
+
+    public String getUser() {
+        return user;
+    }
+
+    public String getMsj() {
+        return msj;
+    }
+
+    public String getH() {
+        return h;
+    }
+
+    public int getImg() {
+        return img;
+    }
 }
--- app/src/main/java/com/example/jotaz/listviewexample/InstagramActivity.java
+++ app/src/main/java/com/example/jotaz/listviewexample/InstagramActivity.java
@@ -2,14 +2,59 @@ package com.example.jotaz.listviewexample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.example.jotaz.listviewexample.InstagramListviewAdapter;
+import com.example.jotaz.listviewexample.Chat;
+import java.util.ArrayList;
+import java.util.List;
 
 public class InstagramActivity extends AppCompatActivity {
+
+    ListView listaChats;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_instagram);
+        this.cargar();
+        this.templateListView();
     }
 
+
+    private void cargar(){
+        listaChats = (ListView) findViewById(R.id.lstChats);
+
+        listaChats.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> a, View v, int position, long id) {
+                String usuarioName = ((Chat)a.getItemAtPosition(position)).getUser();
+                Toast.makeText(InstagramActivity.this,"usuario seleccionado: " + usuarioName, Toast.LENGTH_LONG).show();
+            }
+        });
+
+    }
+
+    private void templateListView(){
+        List<Chat> opc = this.GetElementsToListViewTemplate();
+
+        InstagramListviewAdapter adapter = new InstagramListviewAdapter(this, opc);
+
+        listaChats.setAdapter(adapter);
+    }
+
+    private List<Chat> GetElementsToListViewTemplate()
+    {
+        List<Chat> opc = new ArrayList<>();
+
+        opc.add(new Chat("Eliecer", "Hola como estas", "10 AM",R.drawable.user1));
+        opc.add(new Chat("Maria", "Pasame el problema 3","12 AM",R.drawable.usr2));
+        opc.add(new Chat("Carlos", "Que dieron en HP4 ?","1:50 PM",R.drawable.usr3));
+        opc.add(new Chat("Andrea", "Recuerden el A3 para el miercoles","11:59 PM",R.drawable.usr4));
+
+        return  opc;
+    }
 
 }
